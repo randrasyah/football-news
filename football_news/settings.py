0b1aea1b46a5bcda819b9739780ac35447a35b12
randrasyah <randra.farasya@gmail.com>
2025-09-17 14:23:12 +0700

    fix csrf pws

--- football_news/settings.py
+++ football_news/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "fairuz-akhtar-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://fairuz-akhtar-footballnews.pbp.cs.ui.ac.id/"
+    "https://fairuz-akhtar-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
